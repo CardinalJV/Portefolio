af921b467e730cd6e9913a840f6d2260d42b3006
CardinalJV <jessy.viranaiken@icloud.com>
2024-04-01 17:06:08 +0200

    Mise a jour des styles

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -22,19 +22,34 @@ const skills = () => {
                     </h3>
 
                     <li>
-                        React.js
+                        <h4>
+                            React.js
+                            <p>
+                                #Sass#JSX
+                            </p>
+                        </h4>
                         <span>
                             <FontAwesomeIcon icon={faReact} className='icone' />
                         </span>
                     </li>
                     <li>
-                        Node.js
+                        <h4>
+                            Node.js
+                            <p>
+                                #Express#MongoDB
+                            </p>
+                        </h4>
                         <span>
                             <FontAwesomeIcon icon={faNode} className='icone' />
                         </span>
                     </li>
                     <li>
-                        Git/Github
+                        <h4>
+                            Git/Github
+                            <p>
+                                #Add#Commit
+                            </p>
+                        </h4>
                         <span>
                             <FontAwesomeIcon icon={faGithub} className='icone' />
                         </span>
@@ -43,23 +58,29 @@ const skills = () => {
 
                 <ul>
                     <h3>
-                        Relationnel
+                        Relationnelles
                     </h3>
 
                     <li>
+                        <h4>
                         Adaptabilité
+                        </h4>
                         <span>
                             <FontAwesomeIcon icon={faLightbulb} className='icone' />
                         </span>
                     </li>
                     <li>
+                        <h4>
                         Autonomie
+                        </h4>
                         <span>
                             <FontAwesomeIcon icon={faUser} className='icone' />
                         </span>
                     </li>
                     <li>
+                        <h4>
                         Travail en équipe
+                        </h4>
                         <span><FontAwesomeIcon icon={faUsers} className='icone' /></span>
                     </li>
                 </ul>
